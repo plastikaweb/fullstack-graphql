2b060e09de8d3dce98b7677e8e87fa026377d719
Carlos Matheu Armengol <info@plastikaweb.com>
2018-03-16 15:23:24 +0100

    add apollo dependencies to frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, HttpLinkModule, ApolloModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private apollo: Apollo, private httpLink: HttpLink) {
+    apollo.create({
+      link: httpLink.create({ uri: 'http://localhost:4000/graphql' }),
+      cache: new InMemoryCache()
+    });
+  }
+}
